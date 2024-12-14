4b17be67e5c0cf530da0a84adef0646db53598ba
Adithyan <adithyans2113@gmail.com>
2024-12-14 16:35:10 +0530

    created auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,106 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import 'dotenv/config';   
+import 'dotenv/config';  
+import bcrypt from 'bcrypt'; 
+import User from './Schema/User.js';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 let PORT = 3000;
 
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
-mongoose.connect(process.env.DB_LOCATION, {
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
     autoIndex: true,
 });
 
+const formatDatatoSend = (user) => {
+
+    const access_token = jwt.sign({ id: user ._id }, process.env. SECRET_ACCESS_KEY)
+    return{
+    access_token,    
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname
+    }
+}
+
+const generateUsername = async (email) => {
+
+    let username = email.split("@")[0];
+    
+    let isUsernameExists = await User.exists({ "personal_info.username": username }).then((result) => result)
+    
+    isUsernameExists ? username += nanoid() : "";
+    return username
+    }
+
 app.post('/signup',(req,res) => {
-    res.json(req.body);
+
+    let { fullname, email, password } = req.body;
+    
+    // validating the data from frontend
+    
+    if(!fullname || fullname.length < 3){
+         return res.status(403).json({ "error": "Fullname must be at least 3 letters long" })
+        }
+    if(!email.length) {
+        return res.status(403).json({ "error": "Enter Email" })
+    }
+    if(!emailRegex.test(email)){
+        return res.status(403).json({ "error": "Email is invalid" })
+    } 
+
+    if(!passwordRegex.test(password)){
+        return res.status(403).json({"error":"Password should be 6 to 20 characters long and must contain at least one numeric digit, one uppercase and one lowercase letter"})
+    }
+
+    bcrypt.hash (password, 10, async (err, hashed_password)=> {
+
+        let username = await generateUsername(email);
+
+        let user = new User({
+            personal_info: { 
+                 fullname,
+                 email, 
+                 password: hashed_password,
+                 username 
+                }
+        })
+        user.save().then((u) => {
+            return res.status(200).json(formatDatatoSend(u))
+        }) 
+        .catch(err => {
+            if(err.code === 11000){
+                return res.status(403).json({ "error": "Email already exists" })
+            }
+        })
+});
+});
+
+app.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ "personal_info.email": email });
+        if (!user) {
+            return res.status(403).json({ "error": "Email not found" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.personal_info.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).json({ "error": "Incorrect password" });
+        }
+        return res.status(200).json(formatDatatoSend(user));
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ "error": "Error occurred while logging in, Please try again later" });
+    }
 });
 
 app.listen(PORT, () => {
